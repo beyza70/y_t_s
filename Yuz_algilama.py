ec293ee9a5681ca13ff4a764e4455789b3b8eecf
beyza70 <106872792+beyza70@users.noreply.github.com>
2022-06-08 22:48:21 +0300

    Add files via upload

---
+++ Yuz_algilama.py
@@ -0,0 +1,78 @@
+import os
+from PIL import Image
+import numpy as np
+import cv2
+import pickle
+
+kmr = cv2.VideoCapture(0)
+yuz = cv2.CascadeClassifier('haarcascade-frontalface-default.xml ')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+
+
+
+
+while True:
+    ret,kare = kmr.read()
+
+
+
+    gulme=cv2.CascadeClassifier('haarcascade_smile.xml ')
+
+    gri_renk = cv2.cvtColor(kare,cv2.COLOR_BGR2GRAY)
+    gri_renk_gulme=cv2.cvtColor(kare,cv2.COLOR_BGR2GRAY)
+
+    yuzler = yuz.detectMultiScale(gri_renk,1.5,5)
+
+
+    for(x,y,w,h) in yuzler:
+      cv2.rectangle(kare,(x,y),(x+w,y+h),(0,0,255),2)
+
+      roi_color = kare[y:y + h, x:x + w]
+      roi_gray = gri_renk[y:y + h,x:x+w]
+      id_, dogruluk = recognizer.predict(roi_gray)
+
+
+
+
+     
+      if id_ == 0:
+          print("Beyza")
+          isim = "Beyza"    
+      elif id_ == 1:
+          print("Eren")
+          isim = "Eren"
+      elif id_ == 2:
+          print("Onur")
+          isim = "Onur"
+      
+
+      cv2.putText(kare, isim, (x, y), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+
+      if dogruluk >= 65:
+          print(id_)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    cv2.imshow("Video", kare)
+    if cv2.waitKey(25) & 0xFF==ord('q'):
+        break
+
+kmr.release()
+cv2.destroyAllWindows()
+
---
+++ yuzler.py
@@ -0,0 +1,66 @@
+import os
+from PIL import Image
+import numpy as np
+import cv2
+import pickle
+
+
+yuz = cv2.CascadeClassifier('haarcascade-frontalface-default.xml')
+
+
+#temel klasör dosyalarımızın temelde nerede olduğunun bilgisini verir
+temel_klasor = os.path.dirname(os.path.abspath(__file__))
+resim_klasor=os.path.join(temel_klasor,"resimler")
+#resim klasör resimlerimizin nerede olduğunun bilgisini döndürür
+
+#recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+#recognizer = cv2.createLBPHFaceRecognizer()
+#recognizer=cv2.face.EigenFaceRecognizer_create()
+#x= cv2.createLBHFaceRecognizer()
+y_labels=[]
+x_train=[]
+current_id=0
+label_ids={}
+
+for kok_dizin,dizin,dosyalar in os.walk(resim_klasor):
+ for dosya in dosyalar:
+
+     if dosya.endswith("png") or dosya.endswith("jpg") or dosya.endswith("jpeg"):
+
+         path = os.path.join(kok_dizin,dosya)
+         label=os.path.basename(os.path.dirname(path).replace(" ","-")).lower()
+         print(label,path)
+         y_labels.append(label)
+         x_train.append(path)
+         if not label in label_ids:
+             label_ids[label]=current_id
+             current_id+=1
+
+         id = label_ids[label]
+         print(label_ids)
+
+
+         pil_resim=Image.open(path).convert("L")
+         resim_array = np.array(pil_resim, "uint8")
+
+         yuzler = yuz.detectMultiScale(resim_array, 1.5, 5)
+         for(x,y,w,h) in yuzler:
+             roi=resim_array[y:y+h,x:x+w]
+             x_train.append(roi)
+             y_labels.append(id)
+
+with open("etiket.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_train,np.array(y_labels))
+recognizer.save("trainner.yml")
+
+
+
+
+
+
+
+
